bccee8a7de9f83aa8c313818ddbcf166b59b867b
rushimani2 <158359799+rushimani2@users.noreply.github.com>
2024-02-29 00:06:17 +0530

    baseindex.java

--- index.java
+++ index.java
@@ -1,1 +1,16 @@
-dkffjsl
+class factorial  
+{  
+static int factorialexm(int num){  
+if(num==0)  
+return 1;  
+else   
+return (num * factorialexm(num-1));  
+}  
+public static void main(String args[]){  
+int fact = 1, i;  
+int num = 8;  
+fact = factorialexm(num);  
+System.out.println("Factorial of "+num+" is: "+fact);  
+}  
+}  
+/* commited by developer */
